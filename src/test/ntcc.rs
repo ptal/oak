d1045e45edccc5b13af6c4322926053528016c8e
Pierre Talbot <ptalbot@hyc.io>
2014-07-18 14:53:08 +0200

    [Test] Augment the NTCC grammar and test it.

--- src/test/ntcc.rs
+++ src/test/ntcc.rs
@@ -21,6 +21,7 @@ peg!(
   start = spacing expression
 
   expression = sum
+             / let_in
              / skip_kw
 
   sum
@@ -46,13 +47,32 @@ peg!(
 
   spacing = [" \n\t"]*
 
+  let_in = let_kw var_decl in_kw expression
+
+  var_decl = var_ident eq_bind var_range
+
+  var_range = range
+            / dom_kw var_ident
+
+  // max x .. 10 / min x .. max y / 0..10
+  range = range_bound dotdot range_bound
+
+  range_bound = integer
+              / min_kw var_ident
+              / max_kw var_ident
+
   integer = ["0-9"]+ spacing
-  var_ident = !["0-9"] ["a-zA-Z0-9"]+ spacing
+  var_ident = !["0-9"] ["a-zA-Z0-9_"]+ spacing
 
   pick_kw = "pick" spacing
   when_kw = "when" spacing
   store_kw = "store" spacing
   skip_kw = "skip" spacing
+  let_kw = "let" spacing
+  in_kw = "in" spacing
+  dom_kw = "dom" spacing
+  min_kw = "min" spacing
+  max_kw = "max" spacing
   
   or = "|" spacing
   entail = "|=" spacing
@@ -63,4 +83,6 @@ peg!(
   eq = "==" spacing
   neq = "<>" spacing
   arrow_right = "->" spacing
+  dotdot = ".." spacing
+  eq_bind = "=" spacing
 )
