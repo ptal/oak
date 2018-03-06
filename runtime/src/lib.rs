30438f11b729700ca0b31ac8fbd021191fb9a6bb
MDupas <32423534+MDupas@users.noreply.github.com>
2018-03-06 06:42:46 +0100

    token_stream : to parse on Token (see issue#93) TODO : finish consume_prefix ; convert some tests

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -16,17 +16,20 @@
 //!
 //! This library is used by the generated code of Oak and is also necessary to any Oak users for interfacing with the code generated. A PEG combinator returns a `ParseState`, please consult the methods `into_result` or `unwrap_data` as they are good starting point for retrieving useful information.
 
+#![feature(proc_macro)]
 extern crate syntex_pos;
 
 pub use str_stream::*;
 pub use stream::*;
 pub use parse_state::*;
+pub use token_stream::*;
 use syntex_pos::{BytePos, mk_sp};
 
 pub mod str_stream;
 pub mod parse_state;
 pub mod stream;
 pub mod file_map_stream;
+pub mod token_stream;
 
 pub fn make_span(lo: usize, hi: usize) -> Span {
   mk_sp(
