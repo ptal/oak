c35bac4df0fe2f66d91835192e353ca127a653c6
Pierre Talbot <ptalbot@hyc.io>
2020-12-31 11:50:24 +0100

    [Analysis][WFS] Add a new case for expressions that always succeed without consuming anything.

--- src/liboak/middle/analysis/well_formedness.rs
+++ src/liboak/middle/analysis/well_formedness.rs
@@ -24,6 +24,7 @@ struct WFA
   can_fail: bool,
   can_succeed: bool,
   always_consume: bool,
+  never_consume: bool,
 }
 
 impl WFA
@@ -32,22 +33,24 @@ impl WFA
     WFA {
       can_fail: true,
       can_succeed: true,
-      always_consume: true
+      always_consume: true,
+      never_consume: false
     }
   }
 
-  fn always_succeed() -> Self {
+  fn always_succeed(never_consume: bool) -> Self {
     WFA {
       can_fail: false,
       can_succeed: true,
-      always_consume: false
+      always_consume: false,
+      never_consume
     }
   }
 }
 
-pub struct WellFormedness<'a: 'c, 'b: 'a, 'c>
+pub struct WellFormedness<'a>
 {
-  grammar: &'c AGrammar<'a, 'b>,
+  grammar: &'a AGrammar,
   recursion_path: Vec<(Ident, bool)>,
   consumed_input: bool,
   rules_wfa: HashMap<Ident, WFA>,
@@ -58,9 +61,9 @@ pub struct WellFormedness<'a: 'c, 'b: 'a, 'c>
 
 // Start with an empty set of the expression attributes.
 
-impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
+impl<'a> WellFormedness<'a>
 {
-  pub fn analyse(grammar: AGrammar<'a, 'b>) -> Partial<AGrammar<'a, 'b>> {
+  pub fn analyse(grammar: AGrammar) -> Partial<AGrammar> {
     if WellFormedness::is_well_formed(&grammar) {
       Partial::Value(grammar)
     } else {
@@ -68,13 +71,13 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
     }
   }
 
-  fn is_well_formed(grammar: &'c AGrammar<'a, 'b>) -> bool {
+  fn is_well_formed(grammar: &'a AGrammar) -> bool {
     let mut analyser = WellFormedness::new(grammar);
     analyser.visit_rules();
     analyser.well_formed
   }
 
-  fn new(grammar: &'c AGrammar<'a, 'b>) -> Self {
+  fn new(grammar: &'a AGrammar) -> Self {
     WellFormedness {
       grammar: grammar,
       recursion_path: vec![],
@@ -92,13 +95,13 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
     while !self.reached_fixpoint && self.well_formed {
       self.reached_fixpoint = true;
       for rule in self.grammar.rules.iter() {
-        self.visit_rule(rule.ident());
+        self.visit_rule(&rule.ident());
         if !self.well_formed { break; }
       }
     }
   }
 
-  fn visit_rule(&mut self, rule: Ident) -> WFA {
+  fn visit_rule(&mut self, rule: &Ident) -> WFA {
     if self.is_rec(rule) {
       if !self.consume_input_since(rule) && !self.consumed_input {
         self.error_left_recursion(rule);
@@ -113,13 +116,13 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
     self.rules_wfa[&rule]
   }
 
-  fn visit_rule_expr(&mut self, rule: Ident) -> WFA {
+  fn visit_rule_expr(&mut self, rule: &Ident) -> WFA {
     let expr_idx = self.grammar.expr_index_of_rule(rule);
     self.visit_expr(expr_idx)
   }
 
-  fn push_rule_in_path(&mut self, rule: Ident) {
-    self.recursion_path.push((rule, self.consumed_input));
+  fn push_rule_in_path(&mut self, rule: &Ident) {
+    self.recursion_path.push((rule.clone(), self.consumed_input));
     self.consumed_input = false;
   }
 
@@ -128,25 +131,25 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
     self.consumed_input = old_consumed_input;
   }
 
-  fn fixpoint_update(&mut self, wfa: WFA, rule: Ident) {
+  fn fixpoint_update(&mut self, wfa: WFA, rule: &Ident) {
     if wfa != self.rules_wfa[&rule] {
       self.reached_fixpoint = false;
       *self.rules_wfa.get_mut(&rule).unwrap() = wfa;
     }
   }
 
-  fn is_rec(&self, rule: Ident) -> bool {
-    self.recursion_path.iter().any(|&(r,_)| r == rule)
+  fn is_rec(&self, rule: &Ident) -> bool {
+    self.recursion_path.iter().any(|&(ref r,_)| r == rule)
   }
 
-  fn rec_path_from(&self, rule: Ident) -> Vec<(Ident, bool)> {
+  fn rec_path_from(&self, rule: &Ident) -> Vec<(Ident, bool)> {
     self.recursion_path.iter().cloned()
       .rev()
-      .take_while(|&(r, _)| r != rule)
+      .take_while(|&(ref r, _)| r != rule)
       .collect()
   }
 
-  fn consume_input_since(&self, rule: Ident) -> bool {
+  fn consume_input_since(&self, rule: &Ident) -> bool {
     let mut has_consumed = false;
     for (_, consumed_input) in self.rec_path_from(rule) {
       has_consumed |= consumed_input;
@@ -162,15 +165,15 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
     self.consumed_input = savepoint;
   }
 
-  fn error_left_recursion(&mut self, rule_id: Ident) {
+  fn error_left_recursion(&mut self, rule_id: &Ident) {
     self.well_formed = false;
     let rule = self.grammar.find_rule_by_ident(rule_id);
     if self.register_error(rule.expr_idx) {
-      let mut rec_path: Vec<_> = vec![rule_id];
+      let mut rec_path: Vec<_> = vec![rule_id.clone()];
       rec_path.extend(self.rec_path_from(rule_id).into_iter()
         .map(|(r,_)| r)
         .rev());
-      self.grammar.span_err(rule.span(), format!(
+      rule.span().unstable().error(format!(
         "Left-recursion is not supported in Oak; the following rule cycle \
         do not consume any input and would therefore loop forever\n\
         Detected cycle: {}\n\
@@ -178,27 +181,36 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
         consumes at least one atom in the input before calling \
         the next one. Usually, left-recursion is rewritten with a \
         repeat operator (`e*` or `e+`).",
-        display_path_cycle(&rec_path)));
+        display_path_cycle(&rec_path))).emit();
     }
   }
 
   fn error_never_succeed(&mut self, expr_idx: usize) {
     if self.register_error(expr_idx) {
       self.well_formed = false;
-      self.grammar.span_err(self.grammar[expr_idx].span(), format!(
+      self.grammar[expr_idx].span().unstable().error(format!(
         "Expression will never succeed.\n\
-        Solution: Remove this expression."));
+        Solution: Remove this expression.")).emit();
+    }
+  }
+
+  fn error_always_succeed_without_consuming(&mut self, expr_idx: usize) {
+    if self.register_error(expr_idx) {
+      self.well_formed = false;
+      self.grammar[expr_idx].span().unstable().error(format!(
+        "Expression will always succeed without consuming any input.\n\
+        Solution: Remove this expression.")).emit();
     }
   }
 
   fn error_loop_repeat(&mut self, expr_idx: usize) {
     if self.register_error(expr_idx) {
       self.well_formed = false;
-      self.grammar.span_err(self.grammar[expr_idx].span(), format!(
+      self.grammar[expr_idx].span().unstable().error(format!(
         "Infinite loop detected. A repeat operator (`e*` or `e+`) will \
         never stop because the sub-expression does not consume input.\n\
         Solution: Rewrite the expression such that it consumes at least \
-        one atom in the input or get rid of the repeat operator."));
+        one atom in the input or get rid of the repeat operator.")).emit();
     }
   }
 
@@ -206,14 +218,14 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
   {
     if self.register_error(always_succeed_branch) {
       self.well_formed = false;
-      self.grammar.span_err(self.grammar[choice].span(), format!(
+      self.grammar[choice].span().unstable().error(format!(
         "Unreachable branch in a choice expression. We detected that \
         some branches cannot be reached in this expression.\n\
         Solution: Either remove (or rewrite) this branch or move it \
-        in the end of the choice expression."));
-      self.grammar.span_note(self.grammar[always_succeed_branch].span(), format!(
-        "Branch always succeeding"
-      ));
+        to the end of the choice expression."))
+      .span_note(self.grammar[always_succeed_branch].span().unstable(), format!(
+        "Branch always succeeding"))
+      .emit();
     }
   }
 
@@ -222,22 +234,28 @@ impl<'a, 'b, 'c> WellFormedness<'a, 'b, 'c>
   }
 }
 
-impl<'a, 'b, 'c> ExprByIndex for WellFormedness<'a, 'b, 'c>
+impl<'a> ExprByIndex for WellFormedness<'a>
 {
   fn expr_by_index(&self, index: usize) -> Expression {
     self.grammar.expr_by_index(index).clone()
   }
 }
 
-impl<'a, 'b, 'c> Visitor<WFA> for WellFormedness<'a, 'b, 'c>
+impl<'a> Visitor<WFA> for WellFormedness<'a>
 {
   fn visit_expr(&mut self, this: usize) -> WFA {
     let mut wfa = walk_expr(self, this);
     assert!(wfa.can_fail || wfa.can_succeed,
       "Expression must either fails or succeeds.");
+    assert!(!wfa.always_consume || !wfa.never_consume,
+      "Expression cannot always and never consume at the same time.");
     if wfa.can_fail && !wfa.can_succeed {
       self.error_never_succeed(this);
       wfa.can_succeed = true; // Error-recovery.
+    }
+    else if wfa.can_succeed && !wfa.can_fail && wfa.never_consume {
+      self.error_always_succeed_without_consuming(this);
+      wfa.never_consume = false; // Error-recovery.
     }
     wfa
   }
@@ -247,11 +265,13 @@ impl<'a, 'b, 'c> Visitor<WFA> for WellFormedness<'a, 'b, 'c>
     if literal.is_empty() {
       wfa.can_fail = false;
       wfa.always_consume = false;
+      // NOTE: the empty string is special, and it can be desirable that it always succeeds without consuming, for some patterns in combination with choice.
+      // Therefore, we do not set `never_consume = true`.
     }
     wfa
   }
 
-  fn visit_non_terminal_symbol(&mut self, _this: usize, rule: Ident) -> WFA {
+  fn visit_non_terminal_symbol(&mut self, _this: usize, rule: &Ident) -> WFA {
     self.visit_rule(rule)
   }
 
@@ -271,19 +291,20 @@ impl<'a, 'b, 'c> Visitor<WFA> for WellFormedness<'a, 'b, 'c>
   }
 
   fn visit_zero_or_more(&mut self, this: usize, child: usize) -> WFA {
-    self.visit_repeat(this, child);
-    WFA::always_succeed()
+    let child_wfa = self.visit_repeat(this, child);
+    WFA::always_succeed(child_wfa.never_consume)
   }
 
   fn visit_optional(&mut self, _this: usize, child: usize) -> WFA {
-    self.visit_expr(child);
-    WFA::always_succeed()
+    let child_wfa = self.visit_expr(child);
+    WFA::always_succeed(child_wfa.never_consume)
   }
 
   fn visit_syntactic_predicate(&mut self, _this: usize, child: usize) -> WFA {
     let child_wfa = self.visit_expr(child);
     let mut wfa = child_wfa;
     wfa.always_consume = false;
+    wfa.never_consume = true;
     wfa
   }
 
@@ -297,7 +318,8 @@ impl<'a, 'b, 'c> Visitor<WFA> for WellFormedness<'a, 'b, 'c>
     let mut wfa = WFA {
       can_fail: true,
       can_succeed: false,
-      always_consume: true
+      always_consume: true,
+      never_consume: true
     };
     for i in 0..children.len() {
       let child = children[i];
@@ -307,6 +329,7 @@ impl<'a, 'b, 'c> Visitor<WFA> for WellFormedness<'a, 'b, 'c>
       wfa.can_fail &= child_wfa.can_fail;
       wfa.can_succeed |= child_wfa.can_succeed;
       wfa.always_consume &= child_wfa.always_consume;
+      wfa.never_consume &= child_wfa.never_consume;
       if i != children.len() - 1 && !child_wfa.can_fail {
         self.error_unreachable_branches(this, children[i]);
         return wfa;
@@ -320,13 +343,15 @@ impl<'a, 'b, 'c> Visitor<WFA> for WellFormedness<'a, 'b, 'c>
     let mut wfa = WFA {
       can_fail: false,
       can_succeed: true,
-      always_consume: false
+      always_consume: false,
+      never_consume: true
     };
     for child in children {
       let child_wfa = self.visit_expr(child);
       wfa.can_fail |= child_wfa.can_fail;
       wfa.can_succeed &= child_wfa.can_succeed;
       wfa.always_consume |= child_wfa.always_consume;
+      wfa.never_consume &= child_wfa.never_consume;
       if child_wfa.always_consume {
         self.consumed_input = true;
       }
